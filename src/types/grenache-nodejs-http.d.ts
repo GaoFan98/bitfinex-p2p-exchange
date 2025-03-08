46fec7dc9afcb366dbd0dc5e1f1e2b24aa2bdfaa
vagif <vagifaghayev270298@gmail.com>
2025-03-08 16:21:59 +0900

    fix npm build type errors

--- src/types/grenache-nodejs-http.d.ts
+++ src/types/grenache-nodejs-http.d.ts
@@ -1,6 +1,7 @@
 // refer to https://github.com/bitfinexcom/grenache-nodejs-http
 declare module 'grenache-nodejs-http' {
-    import {Link} from 'grenache-nodejs-link';
+    import { Link } from 'grenache-nodejs-link';
+    import { EventEmitter } from 'events';
 
     export interface PeerRPCServerOptions {
         timeout?: number;
@@ -10,26 +11,36 @@ declare module 'grenache-nodejs-http' {
         timeout?: number;
     }
 
+    export interface RequestOptions {
+        timeout?: number;
+    }
+
+    export interface TransportServer extends EventEmitter {
+        listen(port: number): void;
+        unlisten(): void;
+        on(event: 'request', listener: (rid: string, key: string, payload: unknown, handler: ResponseHandler) => void): this;
+    }
+
+    export interface ResponseHandler {
+        reply(err: Error | null, data?: unknown): void;
+    }
+
     // refer to https://github.com/bitfinexcom/grenache-nodejs-http/blob/master/lib/PeerRPCServer.js
     export class PeerRPCServer {
         constructor(link: Link, options?: PeerRPCServerOptions);
-
         public init(): void;
-
-        public transport(type: 'server'): any;
+        public transport(type: 'server'): TransportServer;
     }
 
     // refer to https://github.com/bitfinexcom/grenache-nodejs-http/blob/master/lib/PeerRPCClient.js
     export class PeerRPCClient {
         constructor(link: Link, options?: PeerRPCClientOptions);
-
         public init(): void;
-
         public request(
             key: string,
             payload: unknown,
-            options: any,
+            options: RequestOptions,
             callback: (err: Error | null, data?: unknown) => void
         ): void;
     }
-}
+} 
--- src/types/grenache-nodejs-link.d.ts
+++ src/types/grenache-nodejs-link.d.ts
@@ -1,18 +1,18 @@
-// refer to https://github.com/bitfinexcom/grenache-nodejs-link
-declare module 'grenache-nodejs-http' {
-    interface LinkOptions {
-        grape: string
+declare module 'grenache-nodejs-link' {
+    export interface LinkOptions {
+        grape: string;
+    }
+
+    export interface AnnounceOptions {
+        timeout?: number;
+        interval?: number;
     }
 
     export class Link {
-        constructor(options: LinkOptions)
-
+        constructor(options: LinkOptions);
         public start(): void;
-
         public stop(): void;
-
-        public announce(name: string, port: number, options: any): void;
-
+        public announce(name: string, port: number, options?: AnnounceOptions): void;
         public lookup(name: string, callback: (err: Error | null, data?: string[]) => void): void;
     }
 
