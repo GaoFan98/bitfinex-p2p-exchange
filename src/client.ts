9ecaa7f317a32d246dc9eb40364b69336c0f6727
vagif <vagifaghayev270298@gmail.com>
2025-03-08 14:14:11 +0900

    client/server communication setup

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,21 @@
 import {getConfig} from './config';
+import {OrderBook} from './models/OrderBook';
+import {P2PService, P2PServiceOptions} from './services/P2PService';
 
 const config = getConfig();
+
+const orderbook = new OrderBook();
+
+const p2pOptions: P2PServiceOptions = {
+    grapeUrl: config.grapeUrl,
+    serviceName: config.serviceName,
+    port: config.nodePort,
+    clientId: config.clientId,
+};
+
+const p2pService = new P2PService(p2pOptions, orderbook, false);
+
+process.on('SIGINT', () => {
+    p2pService.stop();
+    process.exit(0);
+});
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,25 @@
-console.log("server connection")
+import {getConfig} from './config';
+import {OrderBook} from './models/OrderBook';
+import {P2PService, P2PServiceOptions} from './services/P2PService';
+
+const config = getConfig();
+const orderbook = new OrderBook();
+
+const p2pOptions: P2PServiceOptions = {
+    grapeUrl: config.grapeUrl,
+    serviceName: config.serviceName,
+    port: config.nodePort,
+    clientId: config.clientId,
+};
+
+const p2pService = new P2PService(p2pOptions, orderbook, true);
+
+async function main(): Promise<void> {
+    await p2pService.start();
+}
+
+process.on('SIGINT', () => {
+    p2pService.stop();
+});
+
+void main();
