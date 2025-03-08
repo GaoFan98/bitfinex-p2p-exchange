9d03bf695e7766184621ebc5955de505ce7f903f
vagif <vagifaghayev270298@gmail.com>
2025-03-08 14:08:56 +0900

    revert back LOCALHOST_URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,7 +14,7 @@ const LOCALHOST_URL: string = 'http://127.0.0.1:30001'
 
 export function getConfig(): Config {
     const nodeType = (process.env.NODE_TYPE as NodeType) || 'client';
-    const grapeUrl = process.env.GRAPE_URL || 'http://127.0.0.1:30001';
+    const grapeUrl = process.env.GRAPE_URL || LOCALHOST_URL;
     const nodePort = parseInt(process.env.NODE_PORT || '0', 10) ||
         Math.floor(1024 + Math.random() * 1000);
     const clientId = process.env.CLIENT_ID || `node-${randomUUID()}`;
